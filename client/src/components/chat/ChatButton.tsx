b811415d2d010d9589245a1678498f9589109207
sheep_wizard <you@example.com>
2024-10-28 21:20:58 +0000

    chat button

--- client/src/components/chat/ChatButton.tsx
+++ client/src/components/chat/ChatButton.tsx
@@ -1,5 +1,7 @@
 import { RefObject } from "preact";
+import { css } from "../../../styled-system/css";
 import { center } from "../../../styled-system/patterns";
+import ChatIcon from "../icons/ChatIcon";
 
 interface ChatButtonProps {
   onClick: () => void;
@@ -18,8 +20,8 @@ export default function ChatButton({
       onClick={onClick}
       data-bubble={notificationBubble}
       class={center({
-        width: 10,
-        height: 10,
+        width: 16,
+        height: 16,
         bg: "ww-purple",
         borderRadius: "999999px",
         pos: "fixed",
@@ -29,21 +31,37 @@ export default function ChatButton({
         shadow: "ww-mid",
         border: "solid 2px",
         borderColor: "ww-black",
+        mdDown: {
+          width: 10,
+          height: 10,
+        },
         "&[data-bubble=true]": {
           _after: {
             content: '""',
             position: "absolute",
             width: 4,
             height: 4,
-            top: -1,
+            top: 0,
             left: -1,
             borderRadius: 99999999,
             bg: "red",
+            mdDown: {
+              top: -1,
+            },
           },
         },
       })}
     >
-      🗨
+      <ChatIcon
+        class={css({
+          width: 8,
+          height: 8,
+          mdDown: {
+            width: 4,
+            height: 4,
+          },
+        })}
+      />
     </div>
   );
 }
