c8658bb0f9140ce8d697c53d1546fb3e9d0ae5c0
sheep_wizard <you@example.com>
2024-11-28 20:30:09 +0000

    woop

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,13 +2,13 @@ import { lazy, Suspense } from "preact/compat";
 import Chat from "./components/chat/Chat";
 import Help from "./components/help/Help";
 import NotConnectedBanner from "./components/NotConnectedBanner";
-import EndGamePage from "./pages/EndGamePage";
+import LobbyPage from "./pages/lobbyPage/LobbyPage";
 import { useRoom } from "./providers/RoomProvider";
 import { Room } from "./types";
 
-const LobbyPage = lazy(() => import("./pages/lobbyPage/LobbyPage"));
 const GamePage = lazy(() => import("./pages/gamePage/GamePage"));
 const LandingPage = lazy(() => import("./pages/landingPage/LandingPage"));
+const EndGamePage = lazy(() => import("./pages/EndGamePage"));
 
 export function App() {
   const { room } = useRoom();
@@ -26,15 +26,15 @@ export function App() {
 
 function getPage(roomState?: Room["state"]) {
   if (!roomState) {
-    return <LandingPage />;
+    return (
+      <Suspense fallback={<div>loading...</div>}>
+        <LandingPage />
+      </Suspense>
+    );
   }
   switch (roomState) {
     case "lobby":
-      return (
-        <Suspense fallback={<div>loading...</div>}>
-          <LobbyPage />
-        </Suspense>
-      );
+      return <LobbyPage />;
     case "inGame":
       return (
         <Suspense fallback={<div>loading...</div>}>
