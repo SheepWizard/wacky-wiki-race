4076d4cd65cc5cd7255f024650248f47e9b2d3ed
sheep_wizard <you@example.com>
2024-12-10 21:02:34 +0000

    overflow

--- client/src/components/Kawaii.tsx
+++ client/src/components/Kawaii.tsx
@@ -8,6 +8,7 @@ export default function Kawaii() {
         h: "full",
         bg: "radial-gradient(circle, rgba(162,210,255,1) 0%, rgba(205,180,219,1) 100%)",
         pos: "absolute",
+        overflow: "hidden",
       })}
     >
       <img
